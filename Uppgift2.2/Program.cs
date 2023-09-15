ebb3ddccd8f5c659b1b72e056ca9a7ea4830a097
majo06005 <majo06005@E-PW0234K2>
2023-09-15 12:47:06 +0200

    Add project files.

---
+++ Uppgift2.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Uppgift_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt hoppade du Elin i meter?");
+            string Elinshopp = Console.ReadLine();
+            double längdtext1 = double.Parse(Elinshopp);
+            Console.WriteLine("Hur långt Hoppade du Alva i meter?");
+            string Alvashopp = Console.ReadLine();
+            double längdtext2 = double.Parse(Alvashopp);
+            double Skillnad = längdtext1 - längdtext2;
+            Console.WriteLine("Elin hoppade " + Skillnad + " m längre");
+            Console.ReadKey();
+        }
+    }
+}
